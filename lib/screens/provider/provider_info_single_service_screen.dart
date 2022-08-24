8e6382d7b9baf1c92763bd29a1559d31929cfd18
shadownetz <okwuzuchiagozie@gmail.com>
2022-08-24 20:39:27 +0100

    add reviews

--- lib/screens/provider/provider_info_single_service_screen.dart
+++ lib/screens/provider/provider_info_single_service_screen.dart
@@ -10,9 +10,14 @@ import 'package:loader_overlay/loader_overlay.dart';
 
 import '../../blocs/inventory/inventory_event.dart';
 import '../../blocs/inventory/inventory_state.dart';
+import '../../blocs/review/review_bloc.dart';
+import '../../blocs/review/review_event.dart';
+import '../../blocs/review/review_state.dart';
+import '../../models/local/local_review_model.dart';
 import '../../utilities/alerts.dart';
 import '../../utilities/colours.dart';
 import '../../utilities/widgets/provider_widgets.dart';
+import '../user/leave_a_review_screen.dart';
 
 
 class ProviderInfoSingleServiceScreen extends StatefulWidget {
@@ -27,8 +32,10 @@ class ProviderInfoSingleServiceScreen extends StatefulWidget {
 }
 
 class _ProviderInfoSingleServiceScreenState extends State<ProviderInfoSingleServiceScreen> {
-  bool showReviewButton = true;
+  bool showReviewButton = false;
   InventoryModel? _inventory;
+  List<LocalReviewModel>? _reviews;
+  ReviewBloc? _reviewProvider;
 
   @override
   Widget build(BuildContext context) {
@@ -37,9 +44,14 @@ class _ProviderInfoSingleServiceScreenState extends State<ProviderInfoSingleServ
       backgroundColor: Colours.tertiary,
       appBar: portfolioAppBar(context),
       body: CustomLoadingOverlay(
-        widget: BlocProvider(
-          create: (context)=>InventoryBloc()..add(FetchProviderInventories(widget.user)),
-          child: BlocConsumer<InventoryBloc, InventoryState>(
+        widget: MultiBlocProvider(
+          providers: [
+            BlocProvider(
+                create: (context)=>InventoryBloc()..add(FetchProviderInventories(widget.user))
+            ),
+            BlocProvider<ReviewBloc>(create: (context)=>ReviewBloc())
+          ],
+          child: BlocListener<InventoryBloc, InventoryState>(
             listener: (context, state){
               if(state is InventoryLoadingState){
                 context.loaderOverlay.show();
@@ -55,60 +67,102 @@ class _ProviderInfoSingleServiceScreenState extends State<ProviderInfoSingleServ
                 Alerts(context).showToast(state.message);
               }
             },
-            builder: (context, state){
-              return Padding(
-                padding: const EdgeInsets.all(10),
-                child: Stack(
-                  children: [
-                    Column(
+            child: BlocListener<ReviewBloc,ReviewState>(
+              listener: (context, state){
+                if(state is ReviewLoadingState){
+                  context.loaderOverlay.show();
+                }else{
+                  context.loaderOverlay.hide();
+                }
+                if(state is ReviewLoadedState){
+                  setState(()=>_reviews = state.reviews);
+                }
+                if(state is ReviewErrorState){
+                  Alerts(context).showToast(state.message);
+                }
+              },
+              child: Builder(
+                builder: (context){
+                  _reviewProvider = BlocProvider.of<ReviewBloc>(context);
+                  return Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Stack(
                       children: [
-                        // provider stats block
-                        portfolioActionBar(context, provider: widget.user),
-                        const SizedBox(height: 30,),
-                        // Tab section
-                        ProviderPortfolioTab(
-                            tabs: [
-                              ProviderPortfolioTabModel(
-                                  label: "details",
-                                  labelWidget: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(Icons.info, size: 15,),
-                                      Text("Details", style: TextStyle(fontWeight: FontWeight.bold),)
-                                    ],
+                        Column(
+                          children: [
+                            // provider stats block
+                            portfolioActionBar(context, provider: widget.user),
+                            const SizedBox(height: 30,),
+                            // Tab section
+                            ProviderPortfolioTab(
+                                tabs: [
+                                  ProviderPortfolioTabModel(
+                                      label: "details",
+                                      labelWidget: Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: const [
+                                          Icon(Icons.info, size: 15,),
+                                          Text("Details", style: TextStyle(fontWeight: FontWeight.bold),)
+                                        ],
+                                      ),
+                                      child: detailsTabView()
                                   ),
-                                  child: detailsTabView()
-                              ),
-                              ProviderPortfolioTabModel(
-                                  label: "reviews",
-                                  labelWidget: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(Icons.star_border, size: 15,),
-                                      Text("Reviews", style: TextStyle(fontWeight: FontWeight.bold))
-                                    ],
-                                  ),
-                                  child: const PortfolioReviewsTabView(reviews: [],)
-                              )
-                            ],
-                            onClick: (value){
-                              setState((){
-                                showReviewButton = (value == 'details');
-                              });
-                            }
+                                  ProviderPortfolioTabModel(
+                                      label: "reviews",
+                                      labelWidget: Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: const [
+                                          Icon(Icons.star_border, size: 15,),
+                                          Text("Reviews", style: TextStyle(fontWeight: FontWeight.bold))
+                                        ],
+                                      ),
+                                      child: buildReviews()
+                                  )
+                                ],
+                                onClick: (value){
+                                  bool isReviews = value == 'reviews';
+                                  setState((){
+                                    showReviewButton = isReviews;
+                                  });
+                                  if(isReviews && _reviews==null){
+                                    _reviewProvider!.add(FetchReviewsEvent(widget.user, withMetaInfo: true));
+                                  }
+                                }
+                            ),
+                          ],
                         ),
+                        showReviewButton?
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: ElevatedButton(
+                              style: buttonPrimaryLgStyle(),
+                              onPressed: ()async{
+                                var result = await showModalBottomSheet(
+                                    context: context,
+                                    isScrollControlled: true,
+                                    backgroundColor: Colors.transparent,
+                                    builder: (context)=>LeaveAReviewScreen(user: widget.user,)
+                                );
+                                if(result == true){
+                                  _reviewProvider!.add(FetchReviewsEvent(widget.user, withMetaInfo: true));
+                                }
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  Icon(Icons.star_border, size: 20,),
+                                  Text("Leave a review")
+                                ],
+                              )
+                          ),
+                        ):
+                        Container(height: 0,)
                       ],
                     ),
-                    showReviewButton?
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: leaveAReviewButton(context,provider: UserModel.providerInstance()),
-                    ):
-                    Container(height: 0,)
-                  ],
-                ),
-              );
-            },
+                  );
+                },
+              ),
+            ),
           ),
         ),
       ),
@@ -134,6 +188,18 @@ class _ProviderInfoSingleServiceScreenState extends State<ProviderInfoSingleServ
       child: const Text("This provider has no message available for now"),
     );
 
+  }
+
+  Widget buildReviews(){
+    if(_reviews != null){
+      if(_reviews!.isNotEmpty){
+        return PortfolioReviewsTabView(reviews: _reviews!,);
+      }
+    }
+    return Container(
+      alignment: Alignment.center,
+      child: const Text("There are no reviews at this time"),
+    );
   }
 
 
